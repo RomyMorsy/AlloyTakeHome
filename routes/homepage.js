658e2922df5c0ba2a72cb680616e972bfa11fefe
RomyMorsy <romy.morsy@gmail.com>
2024-04-22 00:44:42 -0400

    Update homepage.js

--- routes/homepage.js
+++ routes/homepage.js
@@ -5,7 +5,6 @@ const router = express.Router();
 // Define the route handler for the homepage
 router.get('/', (req,res) => {
     //Render the homepage view
-    console.log('homepage router');
     res.render('homepage', {title: 'Homepage'});
 });
 
